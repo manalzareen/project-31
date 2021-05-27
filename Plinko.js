f5e38c76a30d52c18d4b520c28673388a20c78e5
manalzareen <81857933+manalzareen@users.noreply.github.com>
2021-05-27 20:32:28 +0530

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,18 @@
+class Plinko {
+    constructor(x,y,r){
+        var options={
+            isStatic : true
+        }
+        this.body=Bodies.circle(x,y,r,options);
+        this.r = r;
+        World.add(world,this.body);
+        }
+        display(){
+            var pos = this.body.position;
+            ellipseMode(RADIUS);
+            fill("white");
+            ellipse(pos.x,pos.y,this.r,this.r);
+        }
+
+
+}
